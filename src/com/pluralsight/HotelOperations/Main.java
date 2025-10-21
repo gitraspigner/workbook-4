b57b1145938c3e1f0ba13866bc77796892f2326e
raspi <gitraspigner@gmail.com>
2025-10-21 14:14:52 -0700

    Add hotel booking & unbooking tests

--- src/com/pluralsight/HotelOperations/Main.java
+++ src/com/pluralsight/HotelOperations/Main.java
@@ -10,6 +10,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("----Hotel Operations Test Program----");
         //initialize hotel data
+        Hotel hotel = new Hotel("The Grand Budapest", 5, 5);
         Employee frontDeskEmployee = new Employee(7654111, "Jim Thompson", "Front Desk",
                 20.00, 42.50);
         Room room327 = new Room("King");
@@ -19,17 +20,22 @@ public class Main {
         System.out.println("----Employee Data, Punch-In Time (8AM/8:00)----");
         frontDeskEmployee.punchIn(8.00);
         System.out.println(frontDeskEmployee);
-        System.out.println("----Pre Check In----");
+        System.out.println("----Pre Check In & Booking----");
+        System.out.println(hotel);
         System.out.println(room327);
         System.out.println(reservation1);
         room327.checkIn();
-        System.out.println("----Post Check In----");
+        hotel.bookRoom(1, true);
+        System.out.println("----Post Check In & Booking----");
+        System.out.println(hotel);
         System.out.println(room327);
         room327.checkOut();
-        System.out.println("----Post Check Out----");
+        System.out.println("----Post Check Out (not including Unbooking)----");
         System.out.println(room327);
         room327.cleanRoom();
-        System.out.println("----Post Room Clean----");
+        hotel.unbookRoom(1, true);
+        System.out.println("----Post Room Clean & Unbooking----");
+        System.out.println(hotel);
         System.out.println(room327);
         System.out.println("----Employee Data, Punch-Out Time (5PM/17:00)----");
         frontDeskEmployee.punchOut(17.00);
